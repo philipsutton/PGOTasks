cfe132f4c4bbb33cd04656b4216ae236640ef238
philipsutton <61780391+philipsutton@users.noreply.github.com>
2020-05-28 22:34:57 +0200

    Add files via upload

---
+++ task9_2/src/task9_2/Item.java
@@ -0,0 +1,33 @@
+package task9_2;
+
+public class Item {
+	private int ID;
+	private String name;
+	
+	public Item(int ID, String name) {
+		this.ID=ID;
+		this.name=name;
+	}
+	
+	public int getId() {
+		return ID;
+	}
+	
+	public void setId() {
+		this.ID=ID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName() {
+		this.name=name;
+	}
+	
+	
+	public void show() {
+		System.out.println(this.name);
+	}
+	
+}
